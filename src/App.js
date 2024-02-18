3371a557c5c1c686aeb53750288ed05457feb66c
meanev56 <emekanjoku86@gmail.com>
2024-02-18 07:18:56 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import "./css/style.css";
+import "./css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './common/Navbar';
+import { Home, Cart, Shop } from './pages/index';
+import Footer from './common/Footer';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/shop' element={<Shop />} />
+          <Route path='/cart' element={<Cart />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
---
+++ src/pages/HomePage/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import HomeSlide from '../../components/Slider'
+import Features from '../../components/Features'
+import Products from '../../components/Products'
+
+
+export default function Home () {
+  return (
+    <div>
+      <HomeSlide />
+      <Features />
+      <Products />
+    </div>
+  )
+}
